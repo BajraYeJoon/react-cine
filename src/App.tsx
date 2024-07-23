57ed5075143991e53fcf583ce836bd7cc280a9c8
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-23 14:52:15 +0545

    chore: Add initial project files and configurations

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ModeToggle } from './components/global/mode-toggle'
+
+function App() {
+
+  return (
+    <main>
+      <ModeToggle />
+    </main>
+  )
+}
+
+export default App
