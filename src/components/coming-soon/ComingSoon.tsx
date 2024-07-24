9e7b16bc9d20d1f48d01a444230067eaccb75b81
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-07-24 15:08:51 +0545

    refactor: Update ComingSoon component to use Dialog component for image display

--- src/components/coming-soon/ComingSoon.tsx
+++ src/components/coming-soon/ComingSoon.tsx
@@ -1,5 +1,27 @@
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+
 const ComingSoon = () => {
-  return <div>ComingSoon</div>;
+  return (
+    <section className="px-4">
+      <h1 className="text-4xl tracking-wide font-medium">Coming Soon</h1>
+      <div className="flex  flex-wrap gap-4 mt-4">
+        <Dialog>
+          <DialogTrigger>
+            <div className="h-56 w-44 bg-foreground/25 rounded-3xl"></div>
+          </DialogTrigger>
+
+          <DialogContent>asdfsdf</DialogContent>
+        </Dialog>
+        <div className="h-56 w-44 bg-foreground/25 rounded-3xl"></div>
+
+        <div className="h-56 w-44 bg-foreground/25 rounded-3xl"></div>
+
+        <div className="h-56 w-44 bg-foreground/25 rounded-3xl"></div>
+
+        <div className="h-56 w-44 bg-foreground/25 rounded-3xl"></div>
+      </div>
+    </section>
+  );
 };
 
 export { ComingSoon };
