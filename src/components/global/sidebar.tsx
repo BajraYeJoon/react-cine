051732f69ece89e0198f9d191074155bd60addcd
bajrayejoon <binaya.bajrashakya@gmail.com>
2024-08-01 13:24:34 +0545

    refactor: Improve responsive design and update navbar styling

--- src/components/global/sidebar.tsx
+++ src/components/global/sidebar.tsx
@@ -4,10 +4,10 @@ const Sidebar = () => {
   return (
     <>
       <aside
-        className="fixed hidden h-screen w-64 -translate-x-full transition-transform sm:translate-x-0 md:flex"
+        className="fixed hidden h-screen -translate-x-full transition-transform sm:translate-x-0 md:flex"
         aria-label="Sidebar"
       >
-        <div className="mx-3 flex h-[90vh] flex-col justify-between bg-primary-foreground/10 px-3 py-4">
+        <div className="mx-3 flex h-[90vh] w-56 flex-col justify-between bg-primary-foreground/10 px-3 py-4">
           <Sidebargroup />
         </div>
       </aside>
